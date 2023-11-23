8956af0418f3d411aabe19a42b44509c4d5913d1
AP3YE3 <149599247+AP3YE3@users.noreply.github.com>
2023-11-23 22:39:56 +0800

    23/11/23
    透過freeCodeCamp學習JS。

---
+++ freeCodeCamp/JS範例/Test.js
@@ -0,0 +1,55 @@
+// This is an in-line comment.
+
+/* This is a multi-line comment */
+
+var myName;
+myName = 7;
+
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+
+var a = 9;
+
+var myFirstName = "myFirstName";
+var myLastName = "myLastName";
+
+// 只修改這一行下面的代碼
+var a;
+var b;
+var c;
+
+a = 5;
+b = 10;
+c = "I am a"
+// 只修改這一行上面的代碼
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 變量聲明
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+var studlyCapVar;
+var titleCaseOver;
+
+// 變量賦值
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // 修改這一行
+let fact = "is cool!"; // 修改這一行
+fact = "is awesome!";
+console.log(FCC, fact); // 修改這一行
+
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
